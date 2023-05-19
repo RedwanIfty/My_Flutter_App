21682d7c95a4305551ff473532a456eeac0bab68
RedwanIfty <ifty8555@gmail.com>
2023-05-19 11:11:46 +0600

    19_05_2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' show RaisedButton;
 
-void main() => runApp(MaterialApp(
-  home: Home()
-));
+void main() {
+  runApp(MyApp());
+}
 
-class Home extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('my first app'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
+        title: Text(widget.title),
       ),
       body: Center(
-
-        child: ElevatedButton.icon(
-          onPressed: (){},
-          icon: Icon(
-            Icons.mail
-          ),
-          label: Text('mail me'),
-          // color: Colors.amber,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
-
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('click'),
-        backgroundColor: Colors.red,
-      ) ,
-    )
-    ;
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
